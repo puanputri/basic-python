acbdc15ca40ac00f686c9c40788afd2c914ffea3
fatah-illah <fatahillahera18@gmail>
2024-05-30 09:15:04 +0700

    second commit

--- putri.js
+++ putri.js
@@ -3,5 +3,3 @@ console.log("hello")
 // # basic-python
 // # basic-python
 // # basic-python
-// `git remote set-url origin https://puanputri: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
-// @github.com/puanputri/basic-python.git`
